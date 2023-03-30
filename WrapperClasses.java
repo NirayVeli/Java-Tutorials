ad4109adc3fa73f12871afecd7c22ca1d06dee9d
Niray Veli <63594484+NirayVeli@users.noreply.github.com>
2023-03-30 19:27:04 +0300

    Add files via upload

---
+++ WrapperClasses.java
@@ -0,0 +1,29 @@
+
+public class WrapperClasses {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		// Primitive  -----  Wrapper
+		
+		// boolean    -----  Boolean
+		// int        -----  Integer
+		// double     -----  Double
+		// char       -----  Character
+		
+		
+			Boolean x = true;
+			Integer y = 357;
+			Double z = 32.66;
+			Character a = '$';
+			
+			
+		// Also work as primitive types
+			
+			if(x = true) {
+				System.out.println("This is true");
+			}
+			
+	}
+
+}
