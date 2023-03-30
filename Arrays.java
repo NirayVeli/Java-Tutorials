8f82ab1bdcf954d5d70e0c1f91e53205c2d2dbb9
Niray Veli <63594484+NirayVeli@users.noreply.github.com>
2023-03-30 19:24:00 +0300

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,22 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		// Simple arrays
+		
+	String[] food = {"Fries","Apple","Bread"};
+	
+	food[0] = "Potatoes";
+	
+	System.out.println(food[2]);
+
+	// Different way to use simple array:
+	// String[] food = new String[5];
+	// food[0] = "Lemon"; etc.
+	
+	
+		// For 2D arrays use: String[][] food = new String[5][5];
+	}
+
+}
